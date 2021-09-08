3516aa97a75f79cd6eabe9dd2e3b3d9fc9ba15a2
Marcio Andre Mendes <marcioandre.ti1@gmail.com>
2021-09-08 04:57:57 -0300

    Project created

---
+++ backend/src/main/java/com/devmam/dsvendas2/Dsvendas2Application.java
@@ -0,0 +1,13 @@
+package com.devmam.dsvendas2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsvendas2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsvendas2Application.class, args);
+	}
+
+}
