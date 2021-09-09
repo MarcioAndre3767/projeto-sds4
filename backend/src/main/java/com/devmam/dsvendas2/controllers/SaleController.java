b9eda06db52b4464853c7d4b97fa7fee3c5f8035
Marcio Andre Mendes <marcioandre.ti1@gmail.com>
2021-09-09 06:51:48 -0300

    Pagination

---
+++ backend/src/main/java/com/devmam/dsvendas2/controllers/SaleController.java
@@ -0,0 +1,36 @@
+package com.devmam.dsvendas2.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devmam.dsvendas2.dto.SaleDTO;
+import com.devmam.dsvendas2.service.SaleService;
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+	
+	@Autowired
+	private SaleService service;
+	
+	
+	@GetMapping
+	public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable) {
+		Page<SaleDTO> list = service.findAll(pageable);
+		return ResponseEntity.ok(list);
+	}
+	
+	
+
+}
+
+
+
+
+
+
---
+++ backend/src/main/java/com/devmam/dsvendas2/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devmam.dsvendas2.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devmam.dsvendas2.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
